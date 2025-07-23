a2e1d1f9d7059cb52f44c67b7ce9777cce0e2538
hyejoung <qkrgpwjd1996@gmail.com>
2025-07-23 17:40:45 +0900

    시험문제완료

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+const inputZone = document.querySelector('#input_zone');
+const buttonZone = document.querySelector('button');
+const output = document.querySelector('.output');
+console.log(inputZone, buttonZone, output);
